1469111386e6d60491edc25c0118d2549a0d1483
Polpo26 <49679598+Polpo26@users.noreply.github.com>
2020-06-23 19:52:49 +0200

    Add files via upload

---
+++ polygonGeojson.js
@@ -0,0 +1,26 @@
+var polygonGeojsonFeature = {
+"type": "FeatureCollection",
+"name": "polygonGeojson",
+//"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": 
+ { "info": "20 Adam Street, Dr John Turton (1735-1806), physician with royal patronage; a leading figure in the Royal College of Physicians and physician-in-ordinary to George III and the Prince of Wales from 1797.  With Garrick (at no. 5) he was the first resident of the Terrace." }, "geometry": { 
+     "type": "Polygon", 
+     "coordinates": [ [ [ 
+         [-198.45, 130],[-197.75, 131],[-199.3, 132.2],[-200, 131.15],[-198.45, 130]
+     ] ] ] } },
+{ "type": "Feature", "properties": 
+ { "info": "1 Sir John Mitford (1748-1830), of Lincoln&#39;s Inn and Batsford Park, Gloucestershire. Lawyer and politician, Speaker of the House of Commons 1801 and author on legal and political matters." }, "geometry": { 
+     "type": "Polygon", "coordinates": [ [ [ 
+         [-199.1, 129.2], [-198.45, 130], [-200, 131.15],[-200.65, 130.3], [-199.1, 129.2]
+     ] ] ] } },
+{ "type": "Feature", "properties": 
+ { "info": "2 Walter Cleland (1775-1807). Scottish merchant, who had made a fortune in Calcutta." }, "geometry": { 
+     "type": "Polygon", "coordinates": [ [ [ 
+         [-199.8, 128.4], [-199.1, 129.2], [-200.65, 130.3], [-201.25, 129.5], [-199.8, 128.4]
+     ] ] ] } }
+]
+}
+
+
+           
